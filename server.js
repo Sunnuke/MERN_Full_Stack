fce3f92a3dc86ce29ffd7a26a3cb6b6e8c7e971b
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-22 09:33:31 -0800

    Setup server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Import/Install Express, cors, socket.io
 const express = require("express");
 const cors = require("cors");
-const socket = require("socket.io");
+// const socket = require("socket.io");
 
 // Set Express to a variable (app), and set the selected port to a variable(port), this part is Optional.
 const app = express();
@@ -16,20 +16,20 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Import routes file and address app
-require("./server/routes/ROUTES_NAME_HERE.routes")(app);
+require("./server/routes/pirate.routes")(app);
 
 // Set app to listen for selected port and console.log a message to verify connectivity
 const server = app.listen(port, () => console.log(`server.js: Listening to port ${port}`));
 
 // Socket.io
-const io = socket(server, {cors: true});
+// const io = socket(server, {cors: true});
 
-io.on("connection", socket => {
-    console.log("A user has connected",socket.id);
-    console.log("Nice to meet you. (shake hand)");
-    socket.emit("Hello Human!");
-    socket.on("NAME_EVENT_LISTENER", (data) => {
-        console.log(data);
-        socket.broadcast.emit("NAME_EVENT", {"Example": data.EXAMPLE})
-    })
-});
+// io.on("connection", socket => {
+//     console.log("A user has connected",socket.id);
+//     console.log("Nice to meet you. (shake hand)");
+//     socket.emit("Hello Human!");
+//     socket.on("NAME_EVENT_LISTENER", (data) => {
+//         console.log(data);
+//         socket.broadcast.emit("NAME_EVENT", {"Example": data.EXAMPLE})
+//     })
+// });
